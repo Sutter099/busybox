384157cefe414792261442e0ea9df25b77d5069b
Ze Huang <zehuang@qti.qualcomm.com>
2025-09-10 17:26:04 +0800

    script: add support for ebpf-tools building

--- compile_build_ramdisk.sh
+++ compile_build_ramdisk.sh
@@ -3,10 +3,15 @@
 
 set -euo pipefail # -e: exit immediately if failed；-u: fail if undefined；-o pipefail: any command in a pipeline fails, the whole pipeline fails.
 
+EBPFTOOLS_REPO_URL="https://github.com/Sutter099/ebpf-tools.git"
+EBPFTOOLS_REPO_DIR="ebpf_tools_src"                   # local directory
+EBPFTOOLS_VERSION="example/trace_slab_alloc"                 # git tag
+# INSTALL_DIR="${REPO_DIR}/_install"
+
 REPO_URL="git://git.busybox.net/busybox" # BusyBox
 REPO_DIR="busybox_src"                   # local directory
-BUSYBOX_VERSION="1_37_0"                 # git tag
-INSTALL_DIR="${REPO_DIR}/_install"
+BUSYBOX_VERSION="master"                 # git tag
+INSTALL_DIR="$(pwd)/${REPO_DIR}/_install"
 
 log_info() {
     echo_color green "[INFO] $*"
@@ -33,26 +38,31 @@ echo_color() {
     echo -e "${color_code}${*:2}\033[0m"
 }
 
-clone_or_update_repo() {
-    log_info "checkout BusyBox repo..."
-    if [ -d "$REPO_DIR" ]; then
+clone_or_update_repo_x() {
+    local repo_url="$1"
+    local repo_dir="$2"
+    local repo_tag="$3"
+
+    log_info "checkout ebpf-tools repo..."
+    if [ -d "$repo_dir" ]; then
         log_info "repo already exists"
-        log_info "try reset '$REPO_DIR' to '${BUSYBOX_VERSION}'..."
-        if ! (cd "$REPO_DIR" && git reset --hard "${BUSYBOX_VERSION}" 2>&1); then
+        log_info "try reset '$repo_dir' to '${repo_tag}'..."
+        if ! (cd "$repo_dir" && git reset --hard "${repo_tag}" 2>&1); then
             log_warn "git reset --hard failed, delete and re-clone" && \
-            rm -rf "$REPO_DIR" && \
-            git clone "$REPO_URL" "$REPO_DIR" && \
-            (cd "$REPO_DIR" && git checkout "$BUSYBOX_VERSION") || \
+            rm -rf "$repo_dir" && \
+            git clone "$repo_url" "$repo_dir" && \
+            (cd "$repo_dir" && git checkout "$repo_tag") || \
             log_error "re-clone and checkout failed!"
         fi
         log_info "repo already checkout"
     else
-        log_info "clone to $REPO_DIR..."
-        git clone "$REPO_URL" "$REPO_DIR" || log_error "Git clone failed"
+        log_info "clone to $repo_dir..."
+        git clone "$repo_url" "$repo_dir" && \
+        (cd "$repo_dir" && git submodule update --init --recursive) || log_error "Git clone failed"
     fi
 
     # final check
-    (cd "$REPO_DIR" && git checkout "$BUSYBOX_VERSION") || log_error "Git checkout $BUSYBOX_VERSION failed"
+    (cd "$repo_dir" && git checkout "$repo_tag") || log_error "Git checkout $repo_tag failed"
     log_info "repo ready"
 }
 
@@ -89,10 +99,29 @@ set_busybox_config_option() {
     echo "$new_line" >> .config
 }
 
+build_ebpf_tools() {
+    log_info "start building ebpf-tools..."
+
+    (
+        cd "$EBPFTOOLS_REPO_DIR" || log_error "cannot cd $EBPFTOOLS_REPO_DIR"
+
+        # log_info "clean repo..."
+        # make clean && make -j "$(nproc)" distclean || log_error "clean repo failed"
+
+        log_info "building ebpf-tools..."
+        make -j "$(nproc)" || log_error "ebpf-tools build failed"
+
+        log_info "install to: ($INSTALL_DIR)..."
+        # rm -rf "$INSTALL_DIR" # check it
+        make install DESTDIR=${INSTALL_DIR} || log_error "ebpf-tools install failed!"
+    ) || log_error "ebpf-tools build failed!"
+    log_info "ebpf-tools build complete"
+}
+
 
 build_busybox() {
     log_info "start building BusyBox..."
-    
+
     (
         cd "$REPO_DIR" || log_error "cannot cd $REPO_DIR"
 
@@ -156,6 +185,11 @@ EOF
 # /dev/sda1       /               ext4    defaults        0 1
 EOF
 
+    log_info "create /etc/profile..."
+    cat >"${INSTALL_DIR}/etc/profile" <<'EOF'
+export PATH=$PATH:/usr/local/bin:/usr/local/sbin
+EOF
+
     log_info "create rootfs.cpio..."
     cd "${INSTALL_DIR}" || log_error "cannot cd ${INSTALL_DIR}"
     find . -print0 | cpio --null -ov --format=newc > "../rootfs.cpio" || log_error "create rootfs.cpio failed"
@@ -167,9 +201,12 @@ EOF
 main() {
     log_info "start Initramfs building..."
 
-    clone_or_update_repo
+    # clone_or_update_repo
+    clone_or_update_repo_x "$REPO_URL" "$REPO_DIR" "$BUSYBOX_VERSION"
+    clone_or_update_repo_x "$EBPFTOOLS_REPO_URL" "$EBPFTOOLS_REPO_DIR" "$EBPFTOOLS_VERSION"
 
     build_busybox
+    build_ebpf_tools
 
     generate_initramfs
 
